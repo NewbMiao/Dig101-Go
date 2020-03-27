66142674344b1a946a4b2c0209c8da56c62d5af2
newbmiao <newbvirgil@gmail.com>
2020-03-27 08:34:35 +0800

    update benchmark for struct align

--- types/struct/struct_test.go
+++ types/struct/struct_test.go
@@ -9,30 +9,33 @@ import (
 go get golang.org/x/perf/cmd/benchstat
 
 go test github.com/NewbMiao/Dig101-Go/types/struct -bench .  -count=10 -cpu 1 > old.txt
-benchstat old.txt
+
+benchstat old.txt                                                                      
 name       time/op
-Aligned    0.42ns ±22%
-UnAligned  0.43ns ±19%
+Aligned    0.43ns ±32%
+UnAligned  0.44ns ±34%
 */
 type UnAligned struct {
-	b   [7]byte
-	i64 [8]byte
+	b [15]byte
 }
 type Aligned struct {
-	b   [8]byte
-	i64 [8]byte
+	b [16]byte
 }
 
 func BenchmarkAligned(b *testing.B) {
 	x := Aligned{}
-	tmp := (*int64)(unsafe.Pointer(&x.i64))
+	// fmt.Println(uintptr(unsafe.Pointer(&x)) % 8) // == 0
+
+	tmp := (*int64)(unsafe.Pointer(&x))
 	for i := 0; i < b.N; i++ {
 		*tmp = int64(i)
 	}
 }
 func BenchmarkUnAligned(b *testing.B) {
 	x := UnAligned{}
-	tmp := (*int64)(unsafe.Pointer(&x.i64))
+	// fmt.Println(uintptr(unsafe.Pointer(&x)) % 8) // == 1
+
+	tmp := (*int64)(unsafe.Pointer(&x))
 	for i := 0; i < b.N; i++ {
 		*tmp = int64(i)
 	}
